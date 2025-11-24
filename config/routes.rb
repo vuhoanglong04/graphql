c044bd0da1b6fb92e98d42a0f6f08a4eebcc5fba
Vu Linh Hoang Long <longvulinhhoang@gmail.com>
2025-11-24 14:42:49 +0700

    Test CD

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.production? || Rails.env.development?
   post '/graphql', to: 'graphql#execute'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
